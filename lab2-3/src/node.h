f0526493f6fea91f51b78f51dffb959261c67518
Ilya Afanasyev <affeeal@yandex.ru>
2024-04-04 11:44:15 +0300

    add table draft

--- lab2-3/src/node.h
+++ lab2-3/src/node.h
@@ -2,7 +2,21 @@
 
 namespace parser {
 
-enum class Symbol {};
+enum class RuleFor {
+  kProgram,
+  kRules,
+  kRule,
+  kRuleLHS,
+  kRuleRHS,
+  kExpr,
+  kExpr1,
+  kTerm,
+  kTerm1,
+  kFactor,
+};
+
+enum class Symbol {
+};
 
 struct Node {
   virtual ~Node() {}
